cfc0e7c55be4cf8f814ae19191a30c3a4e41a69c
goodcore1 <goodcore1@goodcore1s-MacBook-Pro.local>
2013-06-24 10:10:39 +0500

    completed purchase order screen and added new files for work order

---
+++ sap/models/Work_Order.h
@@ -0,0 +1,27 @@
+//
+//  Work_Order.h
+//  sap
+//
+//  Created by goodcore1 on 6/24/13.
+//  Copyright (c) 2013 goodcore1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Work_Order : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * order_id;
+@property (nonatomic, retain) NSString * order_type;
+@property (nonatomic, retain) NSDate * start_date;
+@property (nonatomic, retain) NSDate * end_date;
+@property (nonatomic, retain) NSString * priority;
+@property (nonatomic, retain) NSString * word_center;
+@property (nonatomic, retain) NSString * equipment;
+@property (nonatomic, retain) NSNumber * status;
+@property (nonatomic, retain) NSString * serial_number;
+@property (nonatomic, retain) NSDate * last_updated;
+@property (nonatomic, retain) NSString * updated_by;
+
+@end
---
+++ sap/models/Work_Order.m
@@ -0,0 +1,26 @@
+//
+//  Work_Order.m
+//  sap
+//
+//  Created by goodcore1 on 6/24/13.
+//  Copyright (c) 2013 goodcore1. All rights reserved.
+//
+
+#import "Work_Order.h"
+
+
+@implementation Work_Order
+
+@dynamic order_id;
+@dynamic order_type;
+@dynamic start_date;
+@dynamic end_date;
+@dynamic priority;
+@dynamic word_center;
+@dynamic equipment;
+@dynamic status;
+@dynamic serial_number;
+@dynamic last_updated;
+@dynamic updated_by;
+
+@end
