7a22f52a90efc8421d78d819fe3e4cda27f814a0
goodcore1 <goodcore1@goodcore1s-MacBook-Pro.local>
2013-06-24 10:36:30 +0500

    added work order files

---
+++ sap/controllers/WorkOrder.h
@@ -0,0 +1,13 @@
+//
+//  WorkOrder.h
+//  sap
+//
+//  Created by goodcore1 on 6/24/13.
+//  Copyright (c) 2013 goodcore1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WorkOrder : UIView
+
+@end
---
+++ sap/controllers/WorkOrder.m
@@ -0,0 +1,31 @@
+//
+//  WorkOrder.m
+//  sap
+//
+//  Created by goodcore1 on 6/24/13.
+//  Copyright (c) 2013 goodcore1. All rights reserved.
+//
+
+#import "WorkOrder.h"
+
+@interface WorkOrder()
+
+@property (retain, nonatomic) IBOutlet UIView *mainView;
+
+@end
+
+
+@implementation WorkOrder
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+
+@end
